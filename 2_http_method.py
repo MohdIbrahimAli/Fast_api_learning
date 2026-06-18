e4e0dfc34eb37fc12900789b531c9dc210373b09
Mohd Ibrahim Ali <mohammedibrahimali100@gmail.com>
2026-06-18 13:12:18 +0530

    add: comments for better readabililty

--- 2_http_method.py
+++ 2_http_method.py
@@ -4,35 +4,35 @@ from dtos import ProductsDTO
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/")  #Root path for the API
 def root_():
     return "Welcome to learning Fastapi"
 
 @app.get("/products")
 def Products():
-    return products
+    return products  #returning all the products
 
-@app.get("/products/{prod}")
-def Product_search(prod:int):
+@app.get("/products/{product_id}")
+def Product_search(product_id:int):
     for product in products:
-        if prod == product.get("id"):
+        if product_id == product.get("id"):   #information about a single product with their ID
             return product
     else:
         return {"error": "Product not found for the ID"}
 
 @app.post("/create_product/")
-def create_product(data:ProductsDTO):
-    data = data.model_dump()
-    products.append(data)
-    print(data)
-    return {"Success":"Product created successfully", "data":products}
+def create_product(body:ProductsDTO):
+    body = body.model_dump()  #using miodel.dump() to convert the pydantic object to a usable dict
+    products.append(body)
+    print(body)
+    return {"Success":"Product created successfully", "body":products}
 
 @app.put("/update_product/{prod_id}")
-def update_product(data:ProductsDTO, prod_id:int):
-    for index, product in enumerate(products):
+def update_product(body:ProductsDTO, prod_id:int):
+    for index, product in enumerate(products):  #Enumerate gives us 2 values 1-index of value, 2-actual value
         if product.get('id') == prod_id:
-            products[index] = data.model_dump()
-            return {"Success": "Product Updated successfully", "product":data}
+            products[index] = body.model_dump()
+            return {"Success": "Product Updated successfully", "product":body}
         return{"error":"Product not found for the ID"}
 
 @app.delete("/delete_product/{prod_id}")

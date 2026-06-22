022bddeef4e972a4743b87a18b427aa7a80aa879
Mohd Ibrahim Ali <mohammedibrahimali100@gmail.com>
2026-06-22 11:28:49 +0530

    Add: First table user_tasks

--- task_management_app/main.py
+++ task_management_app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI 
 from src.utils.db import Base, engine
+from src.tasks.models import TaskModels
 
 Base.metadata.create_all(engine)
 
-app = FastAPI()
+app = FastAPI(title="Task Management application made using FastAPI")
---
+++ task_management_app/src/tasks/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from src.utils.db import Base
+
+class TaskModels(Base):
+    __tablename__ = "users_tasks"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    is_completed = Column(Boolean)

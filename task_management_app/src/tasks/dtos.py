fc5f9f3ed27384b31661dd79d0c7f03647e96538
Mohd Ibrahim Ali <mohammedibrahimali100@gmail.com>
2026-06-25 12:49:55 +0530

    Improve endpoints for better respoinse and statuses

--- task_management_app/src/tasks/dtos.py
+++ task_management_app/src/tasks/dtos.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class CreateTask(BaseModel):
     title:str
     description:str
     is_completed:bool = False
+
+    model_config = ConfigDict(from_attributes=True)
+
+class ResponseTask(BaseModel):
+    id : int
+    title : str
+    model_config = ConfigDict(from_attributes=True)
--- task_management_app/src/tasks/router.py
+++ task_management_app/src/tasks/router.py
@@ -1,35 +1,37 @@
 from fastapi import APIRouter, Depends, status
+from typing import List
+from sqlalchemy.orm import Session
 from src.tasks import controller
-from src.tasks.dtos import CreateTask
+from src.tasks.dtos import CreateTask, ResponseTask
 from src.utils.db import get_db
 
 task_routes= APIRouter(prefix="/tasks")
 
 
-@task_routes.get("/all_tasks",status_code=status.HTTP_200_OK)
-def get_tasks(db = Depends(get_db)):
+@task_routes.get("/all_tasks", response_model=List[ResponseTask], status_code=status.HTTP_200_OK)
+def get_tasks(db:Session = Depends(get_db)):
     return controller.get_task(db)
 
 
 
-@task_routes.get("/one_task/{id}",status_code=status.HTTP_200_OK)
-def get_task_by_id(id:int, db = Depends(get_db)):
+@task_routes.get("/one_task/{id}", response_model=ResponseTask, status_code=status.HTTP_200_OK)
+def get_task_by_id(id:int, db:Session = Depends(get_db)):
     return controller.get_task_by_id(id,db)
 
 
 
-@task_routes.post("/create",status_code=status.HTTP_201_CREATED)
-def create_task(body:CreateTask, db = Depends(get_db)):
+@task_routes.post("/create",response_model=ResponseTask,status_code=status.HTTP_201_CREATED)
+def create_task(body:CreateTask, db:Session = Depends(get_db)):
     return controller.create_task(body, db)
 
 
 
-@task_routes.put("/update_task/{id}", status_code=status.HTTP_201_CREATED)
-def update_task(id:int, body:CreateTask, db = Depends(get_db)):
+@task_routes.put("/update_task/{id}",response_model=ResponseTask, status_code=status.HTTP_201_CREATED)
+def update_task(id:int, body:CreateTask, db:Session = Depends(get_db)):
     return controller.update_task(id, body, db)
 
 
 
 @task_routes.delete("/delete_task/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(id:int, db = Depends(get_db)):
+def delete_task(id:int, db:Session = Depends(get_db)):
     return controller.delete_task(id, db)
